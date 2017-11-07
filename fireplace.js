59375c9e58191b1bc824c9ae94347b0c58193732
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2017-11-07 16:55:49 +0100

    fire

--- fireplace.js
+++ fireplace.js
@@ -12,12 +12,28 @@ process.on('SIGINT', function () {
 });
 
 // ---- animation-loop
+var time = 0
 setInterval(function () {
   for (var led = 0; led < NUM_LEDS; led++) {
-    pixelData[led] = rgb2Int(255, 0, 0);
+    pixelData[led] = get_random_color()
   }
   ws281x.render(pixelData);
-}, 1000 / 30);
+  ws281x.setBrightness(Math.round(Math.random()*80 + (255 - 80)));
+}, 1000);
+
+function get_random_color() {
+  var colors = [
+    {'r':255, 'g':88, 'b':0 },
+    {'r':247, 'g':148, 'b':0 },
+    {'r':255, 'g':148, 'b':0 },
+    {'r':255, 'g':198, 'b':0 },
+    {'r':255, 'g':232, 'b':0 },
+    {'r':255, 'g':98, 'b':0 },
+    {'r':255, 'g':56, 'b':55 },
+  ]
+  var random_color = colors[Math.round(Math.random()*colors.length)];
+  return rgb2Int(random_color['r'], random_color['g'], random_color['b'])
+}
 
 function rgb2Int(r, g, b) {
   return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
